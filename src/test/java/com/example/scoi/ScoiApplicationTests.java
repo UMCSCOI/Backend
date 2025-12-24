869e7f9fd6d6fbe379657ef37448ad02eee58999
JuHeon <juheon0313@naver.com>
2025-12-24 22:30:49 +0900

    🚀 chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/example/scoi/ScoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
