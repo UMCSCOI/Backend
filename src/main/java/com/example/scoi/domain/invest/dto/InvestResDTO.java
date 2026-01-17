54664dc6e983e0b591faa807825954cbf3045366
seohyunk09 <2022112400@dgu.ac.kr>
2026-01-18 03:52:05 +0900

    ✨ feat: 최대 주문 개수 조회 기초 구조(DTO,SuccessCode, ErrorCode) 추가

--- src/main/java/com/example/scoi/domain/invest/dto/InvestResDTO.java
+++ src/main/java/com/example/scoi/domain/invest/dto/InvestResDTO.java
@@ -1,5 +1,17 @@
 package com.example.scoi.domain.invest.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class InvestResDTO {
     // 응답 DTO 정의
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MaxOrderInfoDTO {
+        private String balance;  // 최대 주문 가능 수량/금액
+    }
 }
---
+++ src/main/java/com/example/scoi/domain/invest/exception/code/InvestSuccessCode.java
@@ -0,0 +1,19 @@
+package com.example.scoi.domain.invest.exception.code;
+
+import com.example.scoi.global.apiPayload.code.BaseSuccessCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum InvestSuccessCode implements BaseSuccessCode {
+    
+    MAX_ORDER_INFO_SUCCESS(HttpStatus.OK,
+            "INVEST200_1",
+            "성공적으로 요청을 처리했습니다.");
+    
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
