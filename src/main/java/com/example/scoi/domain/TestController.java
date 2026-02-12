696b7229a0aae785c9ad4f3d963673cc93715b13
JuHeon <juheon0313@naver.com>
2026-02-12 19:10:08 +0900

    🔥 fix: 빌드되게끔 정상화

--- src/main/java/com/example/scoi/domain/TestController.java
+++ src/main/java/com/example/scoi/domain/TestController.java
@@ -2,7 +2,6 @@ package com.example.scoi.domain;
 
 import com.example.scoi.domain.websocket.enums.RiseOrFall;
 import com.example.scoi.global.util.FcmUtil;
-import com.google.firebase.messaging.FirebaseMessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,14 +24,10 @@ public class TestController {
         RiseOrFall riseOrFall = RiseOrFall.RISE;
 
         // 전체 사용자에게 알림 보내기
-        try {
-            fcmUtil.sendNotificationForDepegging(
-                    code+" 가격 변동 알림",
-                    "평소보다 "+code+" 가격이 "+current+"원으로 약 "+percent+"% "+riseOrFall.name()
-            );
-        } catch (FirebaseMessagingException e){
-            log.error("[ FcmUtil ]: 알림 전송 실패, 에러 코드: {}, 스택 트레이스: {}", e.getMessagingErrorCode(), e.getStackTrace());
-        }
+        fcmUtil.sendNotificationForDepegging(
+                code+" 가격 변동 알림",
+                "평소보다 "+code+" 가격이 "+current+"원으로 약 "+percent+"% "+riseOrFall.name()
+        );
 
         return "test";
     }
